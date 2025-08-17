e65d45a4fe77a552ec4dc0d060fb0f7fdf6811ca
omarsherif0 <omar.sherif22@msa.edu.eg>
2025-08-17 07:32:27 +0300

    cors edit

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || '*',
   credentials: true,
 }));
 app.use(express.json());
